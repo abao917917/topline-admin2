80ac78a86db8f5f440a6b21bde04e55a3d82de50
abao917917 <heba1991@126.com>
2019-09-06 18:33:11 +0800

    导入图片资源

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,13 @@ module.exports = {
     'plugin:vue/essential',
     '@vue/standard'
   ],
+  // 自定义规则选项配置对象
+  // key:规则代号
+  // value:规则说明
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 // 优先查找文件，如果文件找不到则找目录
 // 找到目录，优先加载目录中的index.js
 import router from './router'
-
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
