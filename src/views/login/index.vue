b9b9494cc4f65c24c973ab2c0dc186abff2dffb0
abao917917 <heba1991@126.com>
2019-09-07 02:55:38 +0800

    添加home和login视图组件并配置路由导航

---
+++ src/views/login/index.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <p>login Component</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'AppLogin',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
