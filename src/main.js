686a2aaf29b125f1defa83b066dfbba4605fcc68
abao917917 <heba1991@126.com>
2019-09-17 16:22:45 +0800

    用户登录页面布局

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import App from './App.vue'
 // 优先查找文件，如果文件找不到则找目录
 // 找到目录，优先加载目录中的index.js
 import router from './router'
+import './styles/index.less'
 Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   router,
   render: h => h(App)
 }).$mount('#app')
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,18 +1,83 @@
 <template>
-<div>
-  <p>login Component</p>
-</div>
+  <div class="login-warp">
+    <div class="login-form-warp">
+      <div class="login-head">
+        <img src="./logo_index.png" alt />
+      </div>
+      <div class="login-form">
+        <el-form ref="form" :model="form">
+          <el-form-item>
+            <el-input v-model="form.mobile" placeholder="手机号"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-col :span="12">
+              <el-input v-model="form.code" placeholder="验证码"></el-input>
+            </el-col>
+            <el-col :span="10" :offset="2">
+              <el-button type="primary" @click="handleSendCode">获取验证码</el-button>
+            </el-col>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" class="btn-login" @click="onSubmit">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'AppLogin',
   data () {
-    return {}
+    return {
+      form: {
+        mobile: '13683109553',
+        code: ''
+      }
+    };
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!');
+    },
+    handleSendCode () {
+      const { mobile } = this.form;
+      axios({
+        method: 'GET',
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+      }).then(res => {
+        console.log(res.data);
+      });
+    }
   }
-}
+};
 </script>
 
 <style lang='less' scoped>
-
+.login-warp {
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url(../../assets/login_bg.jpg);
+  background-size: 100%;
+  .login-form-warp {
+    background-color: #fff;
+    padding: 50px;
+    .login-head {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 5px;
+      img {
+        width: 200px;
+      }
+    }
+    .btn-login {
+      width: 100%;
+      border-radius: 10px;
+    }
+  }
+}
 </style>
