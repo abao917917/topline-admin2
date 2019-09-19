197567572404b87df3647977422444e54339d7d4
abao917917 <heba1991@126.com>
2019-09-20 03:43:15 +0800

    用户登录——Element表单验证

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-return */
 /* eslint-disable no-undef */
 /* eslint-disable no-undef */
 <template>
@@ -7,11 +8,16 @@
         <img src="./logo_index.png" alt />
       </div>
       <div class="login-form">
-        <el-form ref="form" :model="form">
-          <el-form-item>
+        <!--表单验证：
+        rules配置验证规则
+        将需要验证的字段通过prop属性配置到el-form-item组件上
+        ref 获取表单组件，可以手动调用表单组件的验证方法
+        -->
+        <el-form :rules="rules" ref="ruleForm" :model="form">
+          <el-form-item prop="mobile">
             <el-input v-model="form.mobile" placeholder="手机号"></el-input>
           </el-form-item>
-          <el-form-item>
+          <el-form-item prop="code">
             <el-col :span="12">
               <el-input v-model="form.code" placeholder="验证码"></el-input>
             </el-col>
@@ -39,36 +45,57 @@ export default {
         mobile: '',
         code: '',
         captchaObj: null
+      },
+      rules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { len: 11, message: '长度必须11位', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '长度6个字符', trigger: 'blur' }
+        ]
       }
     };
   },
   methods: {
     handleLogin () {
+      this.$refs['ruleForm'].validate(vaild => {
+        if (!vaild) {
+          return
+        }
+        this.submitLogin()
+      })
+    },
+    submitLogin () {
       axios({
         method: 'POST',
         url: 'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
         data: this.form
-      }).then(res => { // >=200$<=400的状态码都会进入这里
+      }).then(res => {
+        // >=200$<=400的状态码都会进入这里
         // Element中提供的Message消息提示组件，这也是组件调用的一种形式
         this.$message({
           message: '登录成功',
           type: 'success'
-        })
+        });
         // 建议路由跳转都使用name去跳转，路由传参非常方便
         this.$router.push({
           name: 'home'
         })
-      }).catch(err => { // >=400的状态码都会进入这里
+      }).catch(err => {
+        // >=400的状态码都会进入这里
         // console.dir(err) 打印出来的结果总response中有status
         if (err.response.status === 400) {
           this.$message.error('错了哦，这是一条错误消息');
         }
       })
     },
+
     handleSendCode () {
       const { mobile } = this.form;
       if (this.captchaObj) {
-        return this.captchaObj.verify()
+        return this.captchaObj.verify();
       }
       axios({
         method: 'GET',
@@ -84,21 +111,22 @@ export default {
             new_captcha: true,
             product: 'bind'
           },
-          (captchaObj) => {
-            this.captchaObj = captchaObj
+          captchaObj => {
+            this.captchaObj = captchaObj;
             // 这里可以调用验证实例 captchaObj 的实例方法
             // console.log(captchaObj);
-            captchaObj.onReady(function () {
-              // 验证码ready之后才能调用verify方法显示验证码
-              captchaObj.verify(); // 显示验证码
-            })
+            captchaObj
+              .onReady(function () {
+                // 验证码ready之后才能调用verify方法显示验证码
+                captchaObj.verify(); // 显示验证码
+              })
               .onSuccess(function () {
                 // console.log('验证成功了')
                 const {
                   geetest_challenge: challenge,
                   geetest_seccode: seccode,
                   geetest_validate: validate
-                } = captchaObj.getValidate()
+                } = captchaObj.getValidate();
                 axios({
                   method: 'GET',
                   url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${mobile}`,
@@ -108,9 +136,9 @@ export default {
                     validate
                   }
                 }).then(res => {
-                  console.log(res.data)
-                })
-              })
+                  console.log(res.data);
+                });
+              });
           }
         );
       });
